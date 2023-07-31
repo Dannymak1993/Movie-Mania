f7559fe0677a206f991eaa761de688abcfe1c42e
danny-mak1993 <danny.mak2@gmail.com>
2023-07-31 08:27:52 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <button>-</button>
-      <h1>0</h1>
+      <h1>{0}</h1>
       <button>+</button>
     </div>
   );
