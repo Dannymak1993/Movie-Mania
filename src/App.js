0c7630f14219302a4b19d04954da559fdc0052d1
danny-mak1993 <danny.mak2@gmail.com>
2023-07-31 08:26:20 -0500

    removed components, added buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-      <h1> Name: {props.name}</h1>
-      <h2> Last Name: {props.LastName}</h2>
-      <h2> Age: {props.age}</h2>
-    </>
-  )
-}
-
 const App = () => {
-
   return (
     <div className="App">
-      <Person name='John' LastName='Doe' age='30' />
-      <Person name='Jane' LastName='Die' age={30 - 1} />
-      <Person name='Mary' LastName='Doe' Age='24' />
+      <button>-</button>
+      <h1>0</h1>
+      <button>+</button>
     </div>
   );
 }
