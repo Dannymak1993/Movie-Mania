9751973dc9003a890ca5c6089019e0cd41bfe728
danny-mak1993 <danny.mak2@gmail.com>
2023-07-31 08:23:44 -0500

    added multiple person components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-const Person = (props)=> {
+const Person = (props) => {
   return (
     <>
-    <h1> Name: {props.name}</h1>
-    <h2> Last Name: {props.LastName}</h2>
-    <h2> Age: {props.age}</h2>
+      <h1> Name: {props.name}</h1>
+      <h2> Last Name: {props.LastName}</h2>
+      <h2> Age: {props.age}</h2>
     </>
   )
 }
@@ -14,11 +14,9 @@ const App = () => {
 
   return (
     <div className="App">
-      <Person name = {'John'} LastName = {'Doe'} Age = {'30'}/>
-      <Person />
-      <Person />
-      <Person />
-      <Person />
+      <Person name='John' LastName='Doe' age='30' />
+      <Person name='Jane' LastName='Die' age={30 - 1} />
+      <Person name='Mary' LastName='Doe' Age='24' />
     </div>
   );
 }
