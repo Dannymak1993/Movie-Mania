5a1bb4765b406e18697e7c8e140a4fbf1096d970
danny-mak1993 <danny.mak2@gmail.com>
2023-08-02 23:00:56 -0500

    changed placeholder text in app.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
                 <input
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
-                    placeholder="Search for movies"
+                    placeholder="Search for your favorite movies"
                 />
                 <img
                     src={SearchIcon}
